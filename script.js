8872bdca819e1c99089f99768710486f4fe91a77
Spencer Kimball <spencercowleykimball@gmail.com>
2026-03-31 08:52:30 -0600

    Answered the homework questions, added a background animation, as well as a hidden passive aggressive comment if the use is bothered by the weather forecast being so off.

--- script.js
+++ script.js
@@ -10,28 +10,34 @@ let temp_color;
 // For creating a new div for each day
 let element;
 
-
 // Generate 5 random numbers between 0 and 100
 let weather_temps = [];
 
+ // This is a built in function that lets you get todays date.
+let day = new Date();
 
+// Set a container equal to the div container for the new weather cards
+let weather_card_container = document.querySelector("#card_container");
+
+// Forecast date variable
+let forecast_date;
 
 // Change the color based on where the temperature falls
 function check_temps(weather_temps_curr) {
-    // Set a container equal to the div container for the new weather cards
-    let weather_card_container = document.querySelector("#card_container");
+    day = new Date();
 
     weather_card_container.innerHTML = "";
 
     for (let i = 0; i < 5; i++) {
-        weather_temps.push(Math.floor(Math.random() * 101));
+        weather_temps_curr[i] = Math.floor(Math.random() * 101);
     }
 
 
     // Need to create a new div card for each temp value in the weather temps list
     for (let temp of weather_temps_curr) {
+        
         element = document.createElement("div");
-
+        
         // Check what the temp value is and set the background color and weather category
         if(temp <= 60) {
             weather_category = "Cold";
@@ -44,8 +50,18 @@ function check_temps(weather_temps_curr) {
             temp_color = "orange";
         }
 
+        forecast_date = day;
+        
+        forecast_date.setDate(day.getDate() + 1);
+
+        forecast_date = forecast_date.toLocaleDateString("en-US", {
+            month: "short",
+            day: "numeric",
+            year: "numeric"
+        });
+
         // Set the text of the new div to the weather category value
-        element.innerText = weather_category + ": " + temp + " degrees";
+        element.innerText = forecast_date + "\n" + weather_category + ": " + temp + " degrees";
 
         // Set the background color and other styling elements of the new div to the temp color, etc.
         element.style.backgroundColor = temp_color;
@@ -53,7 +69,8 @@ function check_temps(weather_temps_curr) {
         element.style.width = "160px";
         element.style.height = "100px";
         element.style.fontSize = "20px";
-        element.style.marginTop = "20px";
+        element.style.marginTop = "10px";
+        element.style.marginBottom = "10px";
         element.style.display = "flex";
         element.style.justifyContent = "center";
         element.style.padding = "20px";
@@ -65,17 +82,8 @@ function check_temps(weather_temps_curr) {
         weather_card_container.appendChild(element);
 
     }
-    
-}
 
-function reset_divs() {
-    let weather_card_container = document.querySelector("#card_container");
-
-    while (weather_card_container.firstChild) {
-        weather_card_container.removeChild(weather_card_container.firstChild);
-    }
-
-    let weather_temps = [];
+    console.log(forecast_date);
 }
 
 // Had to put the addEventListener into a function?
@@ -84,6 +92,3 @@ check_weather.addEventListener("click", function() {
     check_temps(weather_temps);
 });
 
-clear_weather.addEventListener("click", function() {
-    reset_divs();
-});
